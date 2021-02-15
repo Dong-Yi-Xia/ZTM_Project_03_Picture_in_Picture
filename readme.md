e466265d087a5e7c82bf48bd52202c7aaea94871
Dong-Yi-Xia <dong7777xia@gmail.com>
2021-02-15 16:49:15 -0500

    stopcapture function

--- readme.md
+++ readme.md
@@ -5,6 +5,16 @@
 MDN API <br>
 https://developer.mozilla.org/en-US/docs/Web/API <br>
 
-Guide <br>
+Guide into Picture in Picture<br>
 https://css-tricks.com/an-introduction-to-the-picture-in-picture-web-api/ <br>
 
+Screen capture API <br>
+https://developer.mozilla.org/en-US/docs/Web/API/Screen_Capture_API/Using_Screen_Capture <br>
+Capturing screen contents as a live MediaStream is initiated by calling navigator.mediaDevices.getDisplayMedia(), which returns a promise that resolves to a stream containing the live screen contents. <br>
+
+Navigator.mediaDevices <br>
+https://developer.mozilla.org/en-US/docs/Web/API/Navigator/mediaDevices <br>
+Provides access to connected media input devices like cameras and microphones, as well as screen sharing.<br>
+
+HTMLMediaElement <br>
+https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/loadedmetadata_event <br>
--- script.js
+++ script.js
@@ -5,8 +5,9 @@ const sourceButton = document.getElementById('sourceButton')
 //Prompt to select media stream, pass to video element, then play
 async function selectMediaStream() {
     try {
-        const mediaStream = await navigator.mediaDevices.getDisplayMedia()
-        videoElement.srcObject = mediaStream // HTMLMediaElement
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia() //permission of screenshare
+        videoElement.srcObject = mediaStream // store the connected video element into srcObject 
+        // display the screenshare video in the video element 
         videoElement.onloadeddata = () => {
             videoElement.play()
         }
@@ -16,17 +17,17 @@ async function selectMediaStream() {
     }
 }
 
-
 sourceButton.addEventListener('click', selectMediaStream)
 
+
 startButton.addEventListener('click', async () => {
-    if (document.pictureInPictureElement) {
+    if (document.pictureInPictureElement) { //if doc has pic-in-pic ON, exit pic-in-pic 
         await document
           .exitPictureInPicture()
           .catch(error => {
             // Error handling
           })
-      } else {
+      } else {              //if doc pic-in-pic is OFF, enter pic-in-pic mode
         await videoElement
         .requestPictureInPicture()
         .catch(error => {
@@ -35,14 +36,23 @@ startButton.addEventListener('click', async () => {
       }
 })
 
+//Stop the screenshare and set the video srcObject = null
+function stopCapture(evt) {
+    let tracks = videoElement.srcObject.getTracks();
+    tracks.forEach(track => track.stop());
+    videoElement.srcObject = null;
+}
 
+
+//video event listener
 videoElement.addEventListener('enterpictureinpicture', () => {
     startButton.innerText = 'STOP';
 });
 
 
-videoElement.addEventListener('leavepictureinpicture', () => {
+videoElement.addEventListener('leavepictureinpicture', () => { //exit picture-in-picture mode
     startButton.innerText = 'START';
+    stopCapture() //Stop the screenshare
 });
 
 
